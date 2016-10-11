a19f006b43edfbfbf50bb2fca8314b608cb5ae3e
David Tinianow <david.tinianow@gmail.com>
2016-10-11 17:52:23 -0600

    Give slime default attributes and begin to build move function

--- test/slime-test.js
+++ test/slime-test.js
@@ -8,18 +8,47 @@ describe('Slime', function() {
       let slime = new Slime;
       assert.isObject(slime);
     })
-    it('should a default radius', function() {
-      let attributes = {x: 0, y: 1, speed: 3, color: 'red'};
-      let slime = new Slime(attributes);
+    it('should have a default radius', function() {
+      let slime = new Slime;
       assert.equal(slime.radius, 80);
     })
+    it('should have a default x-coordinate', function() {
+      let slime = new Slime;
+      assert.equal(slime.x, 275);
+    })
+    it('should have a default y-coordinate', function() {
+      let slime = new Slime;
+      assert.equal(slime.y, 600);
+    })
+    it('should have a default color', function() {
+      let slime = new Slime;
+      assert.equal(slime.color, 'red');
+    })
+    it('should have a default speed', function() {
+      let slime = new Slime;
+      assert.equal(slime.speed, 3);
+    })
     it('should have given attributes', function() {
+      let attributes = {x: 1, y: 1, speed: 4, color: 'blue'};
+      let slime = new Slime(attributes);
+      assert.equal(slime.x, 1);
+      assert.equal(slime.y, 1);
+      assert.equal(slime.speed, 4);
+      assert.equal(slime.color, 'blue');
+    })
+    it('should be able to move', function(){
+      let slime = new Slime;
+      assert.isFunction(slime.move);
+    })
+  })
+})
+
+describe('Move', function() {
+  context('Movement', function() {
+    it('should change x-coordinates', function() {
       let attributes = {x: 0, y: 1, speed: 3, color: 'red'};
       let slime = new Slime(attributes);
-      assert.equal(slime.x, 0);
-      assert.equal(slime.y, 1);
-      assert.equal(slime.speed, 3);
-      assert.equal(slime.color, 'red');
+      assert.isFunction(slime.move);
     })
   })
 })
